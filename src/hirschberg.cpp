d52d0b9ec7beb8f58e5ff648d4ec046b7c30237c
iamarchit <saxenaarchit1995@gmail.com>
2022-04-24 23:38:17 -0400

    Archit hirschberg

--- src/hirschberg.cpp
+++ src/hirschberg.cpp
@@ -7,52 +7,58 @@
 */
 #include<iostream>
 #include<fstream>
-#include<vector>
-#include<algorithm>
 using namespace std;
 
-void pretty_print(vector<char> &a_X,vector<char> &a_Y)
+void pretty_print(char *a_X, char *a_Y,uint64_t pos)
 {
     ofstream out_file; 
     out_file = ofstream("output.txt");
     
-    for(uint64_t i=0;i < a_X.size(); i++)
+    for(uint64_t i=0;i < pos; i++)
         out_file << a_X[i];
     out_file << endl;
-    for(uint64_t i=0;i < a_X.size(); i++)
+    for(uint64_t i=0;i < pos; i++)
         out_file << (((a_X[i]!='_' && a_Y[i]!='_') && (a_X[i] != a_Y[i]))?\
         '|' : ' ');
     out_file << endl;
-    for(uint64_t i=0;i < a_X.size(); i++)
+    for(uint64_t i=0;i < pos; i++)
         out_file << a_Y[i];
     
     out_file.close();
 }
 
 void find_edit1(uint64_t i_x, uint64_t j_x,uint64_t i_y, uint64_t j_y,
-                vector<uint64_t>&dp, string &X, string &Y)
+                uint64_t split, uint64_t *dp1, uint64_t *dp2,
+		string &X, string &Y)
 {
     uint64_t temp1,temp2,len1 = j_x - i_x + 1,len2 = j_y - i_y + 1,k,l;
 
-    dp[0] = 0;
+    dp1[0] = 0;
     for(k = 1; k <= len2; k++)
-        dp[k] = k;
+        dp1[k] = k;
 
     for(k=1;k<=len1;k++)
     {
         temp1 = k-1;
-        dp[0] = k;
+        dp1[0] = k;
         for(l=1;l<=len2;l++){
-            temp2 = dp[l];
-            dp[l] = (X[i_x + k -1]==Y[i_y + (l - 1)]) ? temp1 : 1 + min(dp[l-1],min(dp[l],\
+            temp2 = dp1[l];
+            dp1[l] = (X[i_x + k -1]==Y[i_y + (l - 1)]) ? temp1 : 1 + min(dp1[l-1],min(dp1[l],\
             temp1));
             temp1 = temp2;
         }
+	if(i_x + (k - 1) == split)
+        {
+                dp2[0] = k;
+                for(l=1;l<=len2;l++)
+                        dp2[l] = dp1[l];
+        }
+
     }
 }
 
 void find_edit2(uint64_t i_x, uint64_t j_x,uint64_t i_y, uint64_t j_y,
-                vector<uint64_t>&dp, string &X, string &Y)
+                uint64_t *dp, string &X, string &Y)
 {
     uint64_t temp1,temp2,len1 = j_x - i_x + 1,len2 = j_y - i_y + 1,k,l;
 
@@ -68,23 +74,26 @@ void find_edit2(uint64_t i_x, uint64_t j_x,uint64_t i_y, uint64_t j_y,
             temp2 = dp[l];
             dp[l] = (X[j_x - k +1]==Y[j_y - l + 1]) ? temp1 : 1 + min(dp[l-1],min(dp[l],\
             temp1));
+	    
             temp1 = temp2;
         }
     }
 
 }
-void h_recursive(vector<char> &a_X, vector<char>&a_Y,
+
+void h_recursive(char *a_X, char *a_Y,
                  string &X, string &Y,
-                 uint64_t s_x,uint64_t e_x,uint64_t s_y,uint64_t e_y)
+                 uint64_t s_x,uint64_t e_x,uint64_t s_y,uint64_t e_y, uint64_t * pos,
+		 uint64_t *dp1, uint64_t *dp2, uint64_t *dp3)
 {
     uint64_t row = e_x - s_x + 1, col = e_y - s_y + 1,split = (e_x + s_x)/2,k;
-
     if(col==0)
     {
         for(uint64_t i = s_x; i<=e_x; i++)
         {
-            a_X.push_back(X[i]);
-            a_Y.push_back('_');
+            a_X[*pos] = X[i];
+            a_Y[*pos] = '_';
+	    ++*pos;
         }
         return;
     }
@@ -92,8 +101,9 @@ void h_recursive(vector<char> &a_X, vector<char>&a_Y,
     {
         for(uint64_t i = s_y; i<=e_y; i++)
         {
-            a_X.push_back('_');
-            a_Y.push_back(Y[i]);
+            a_X[*pos] = '_';
+            a_Y[*pos] = Y[i];
+	    ++*pos;
         }
         return;
     }
@@ -104,29 +114,32 @@ void h_recursive(vector<char> &a_X, vector<char>&a_Y,
         {
             if(X[s_x]==Y[i])
             {
-                a_X.push_back(X[s_x]);
-                a_Y.push_back(Y[i]);
+                a_X[*pos] = X[s_x];
+                a_Y[*pos] = Y[i];
                 i++;
+		++*pos;
                 break;
             }
             else
             {
                 if(i==e_y)
                 {
-                    a_X.push_back(X[s_x]);
-                    a_Y.push_back(Y[i]);
+                    a_X[*pos] = X[s_x];
+                    a_Y[*pos] = Y[i];
                 }
                 else
                 {
-                    a_X.push_back('_');
-                    a_Y.push_back(Y[i]);
+                    a_X[*pos] = '_';
+                    a_Y[*pos] = Y[i];
                 }
+		++*pos;
             }
         }
         for(; i<=e_y; i++)
         {
-            a_X.push_back('_');
-            a_Y.push_back(Y[i]);
+            a_X[*pos] = '_';
+            a_Y[*pos] = Y[i];
+	    ++*pos;
         }
         return;
     }
@@ -138,40 +151,40 @@ void h_recursive(vector<char> &a_X, vector<char>&a_Y,
         {
             if(X[i]==Y[s_y])
             {
-                a_X.push_back(X[i]);
-                a_Y.push_back(Y[s_y]);
+                a_X[*pos] = X[i];
+                a_Y[*pos] = Y[s_y];
                 i++;
+		++*pos;
                 break;
             }
             else
             {
                 if(i==e_x)
                 {
-                    a_X.push_back(X[i]);
-                    a_Y.push_back(Y[s_y]);
+                    a_X[*pos] = X[i];
+                    a_Y[*pos] = Y[s_y];
                 }
                 else
                 {
-                    a_X.push_back(X[i]);
-                    a_Y.push_back('_');
+                    a_X[*pos] = X[i];
+                    a_Y[*pos] = '_';
                 }
+		++*pos;
             }
         }
         for(; i<=e_x; i++)
         {
-            a_X.push_back(X[i]);
-            a_Y.push_back('_');
+            a_X[*pos] = X[i];
+            a_Y[*pos] = '_';
+	    ++*pos;
         }
         return;
     }
 
     uint64_t ans;
-    vector<uint64_t>dp1(col+1),dp2(col+1),dp3(col+1);
 
-    find_edit1(s_x,e_x,s_y,e_y,dp1,X,Y);
+    find_edit1(s_x,e_x,s_y,e_y,split,dp1,dp2,X,Y);
     ans = dp1[col];
-
-    find_edit1(s_x,split,s_y,e_y,dp2,X,Y);
     find_edit2(split+1,e_x,s_y,e_y,dp3,X,Y);
 
     for(k=0;k<=col;k++)
@@ -180,27 +193,37 @@ void h_recursive(vector<char> &a_X, vector<char>&a_Y,
             break;
     }
     
-    h_recursive(a_X,a_Y,X,Y,s_x,split,s_y, s_y + k - 1);
-    h_recursive(a_X,a_Y,X,Y,split+1,e_x,s_y + k, e_y);
+    h_recursive(a_X,a_Y,X,Y,s_x,split,s_y, s_y + k - 1,pos,dp1,dp2,dp3);
+    h_recursive(a_X,a_Y,X,Y,split+1,e_x,s_y + k, e_y,pos,dp1,dp2,dp3);
 
 }
 void hirschberg(string &X, string &Y)
 {
-    vector<char> a_X,a_Y;
-    
-    h_recursive(a_X,a_Y,X,Y,0,X.size()-1,0, Y.size()-1);
-    pretty_print(a_X,a_Y);
+    uint64_t pos = 0, a_size = X.size() + Y.size();
+    uint64_t *dp1 = (uint64_t *)malloc((a_size + 1) * sizeof(uint64_t));
+    uint64_t *dp2 = (uint64_t *)malloc((a_size + 1) * sizeof(uint64_t));
+    uint64_t *dp3 = (uint64_t *)malloc((a_size + 1) * sizeof(uint64_t));
+    char *a_X = (char *)malloc(a_size * sizeof(char));
+    char *a_Y = (char *)malloc(a_size * sizeof(char));
+    h_recursive(a_X,a_Y,X,Y,0,X.size()-1,0, Y.size()-1, &pos, dp1,dp2,dp3);
+    pretty_print(a_X,a_Y,pos);
+    free(a_X);free(a_Y);free(dp1);free(dp2);free(dp3);
 }
 
 /*
  *  Main function. To run this code. Generates output.txt same as out.txt  
 */
 int main(){
-    ifstream input_file;
+    ifstream input_file1, input_file2;
     string X,Y;
-    input_file = ifstream("in.txt");
-    input_file >> X >> Y;
+    //input_file1 = ifstream("xaa");
+    //input_file2 = ifstream("xab");
+    input_file1 = ifstream("xaa");
+    input_file2 = ifstream("xab");    
+    input_file1 >> X;
+    input_file2 >> Y;
     hirschberg(X,Y);
-    input_file.close();
+    input_file1.close();
+    input_file2.close();
     return 0;
 }
